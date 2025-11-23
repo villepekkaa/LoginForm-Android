47c0bb16a426fe448d790a028797d4339ba1dace
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-11-23 10:06:14 +0200

    valmista tuli

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.loginform.ui.theme.LoginFormTheme
@@ -39,34 +49,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginForm(modifier: Modifier = Modifier) {
-    val appModifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+fun LoginForm(modifier: Modifier = Modifier.background(MaterialTheme.colorScheme.surface)) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp)
+        .padding(vertical = 8.dp)
+
     Column(
-        modifier = modifier
+        modifier = modifier.background(MaterialTheme.colorScheme.surface)
     ) {
         Text(
-            modifier = appModifier.fillMaxWidth(),
+            modifier = appModifier
+                .fillMaxWidth(),
             text = "Login",
-            style = MaterialTheme.typography.titleLarge.copy(fontSize = 28.sp),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary,
             textAlign = androidx.compose.ui.text.style.TextAlign.Center
         )
         OutlinedTextField(
             modifier = appModifier,
             label = { Text("Username") },
-            value = "",
-            onValueChange = { /*TODO*/ }
+            trailingIcon = { Icon(imageVector = Icons.Filled.Email, contentDescription = "Email icon") },
+            value = username,
+            onValueChange = { username = it },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
         )
         OutlinedTextField(
             modifier = appModifier,
             label = { Text("Password") },
-            value = "",
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                onValueChange = { /*TODO*/ }
-            )
+            trailingIcon = { Icon(imageVector = Icons.Filled.Lock, contentDescription = "Password icon") },
+            value = password,
+            onValueChange = { password = it },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
         Button(
             modifier = appModifier,
-            onClick = {/*TODO*/ }
+            onClick = {/*TODO:*/ }
         ) {
             Text("Submit")
         }
